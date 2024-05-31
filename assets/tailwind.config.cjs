bbbc115167d3da94e7dcac1e56c84b70be214f49
Sivritkin Dmitriy <dmitriysivritkin@gmail.com>
2024-05-31 22:06:18 +0300

    refactor: :recycle: add env file, improve tailwind config

--- assets/tailwind.config.cjs
+++ assets/tailwind.config.cjs
@@ -1,13 +1,8 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./src/**/**/*.{js,ts,jsx,tsx}",
-    "./src/**/**/**/*.{js,ts,jsx,tsx}",
-    "./src/**/**/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: ['class'],
+  prefix: '',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
@@ -15,21 +10,20 @@ module.exports = {
           500: '#F6F49D',
         },
         softRed: {
-          500: '#FF7676'
+          500: '#FF7676',
         },
         softGreen: {
-          500: '#5DAE8B'
+          500: '#5DAE8B',
         },
         softBlue: {
-          500: '#466C95'
-        }
+          500: '#466C95',
+        },
       },
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [require("tw-elements/dist/plugin.cjs"), require('tailwindcss-dark-mode')()],
-  darkMode: "class",
-  variants: ['dark', 'dark-hover', 'dark-group-hover', 'dark-even', 'dark-odd']
-}
+  plugins: [require('tw-elements/dist/plugin.cjs'), require('tailwindcss-dark-mode')()],
+  variants: ['dark', 'dark-hover', 'dark-group-hover', 'dark-even', 'dark-odd'],
+};
