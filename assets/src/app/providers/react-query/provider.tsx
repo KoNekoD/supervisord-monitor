9182183dca41a406a7ad1870e5440b270922bc5a
Sivritkin Dmitriy <dmitriysivritkin@gmail.com>
2024-05-31 23:38:21 +0300

    feat: :sparkles: add react-query provider

---
+++ assets/src/app/providers/react-query/provider.tsx
@@ -0,0 +1,11 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+
+export const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => (
+  <QueryClientProvider client={queryClient}>
+    {children}
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
+);
