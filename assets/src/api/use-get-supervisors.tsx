4c98ca91f494afef7b3ae5d1227eb35b54fbdf79
Sivritkin Dmitriy <dmitriysivritkin@gmail.com>
2024-05-31 22:53:43 +0300

    refactor: :recycle: delete magic strings, improve code quality

--- assets/src/api/use-get-supervisors.tsx
+++ assets/src/api/use-get-supervisors.tsx
@@ -1,4 +1,5 @@
 import { $api } from '~/shared/api';
+import { API_ENDPOINTS } from '~/shared/const';
 
 export const getSupervisors = (): Promise<ApiSupervisor[]> =>
-  $api.get<ApiSupervisor[]>(`/supervisors`).then(response => response.data);
+  $api.get<ApiSupervisor[]>(API_ENDPOINTS.SUPERVISORS()).then(response => response.data);
--- assets/src/api/use-manage-supervisors.tsx
+++ assets/src/api/use-manage-supervisors.tsx
@@ -1,4 +1,5 @@
 import { $api } from '~/shared/api';
+import { API_ENDPOINTS } from '~/shared/const';
 
 export const manageSupervisor = (manage: ApiSupervisorSupervisorManage): Promise<ApiSupervisorSupervisorManageResult> =>
-  $api.post<ApiSupervisorSupervisorManageResult>(`/supervisors/manage`, manage).then(response => response.data);
+  $api.post<ApiSupervisorSupervisorManageResult>(API_ENDPOINTS.MANAGE_SUPERVISORS(), manage).then(response => response.data);
