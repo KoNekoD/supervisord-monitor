63ad43de10fbb893b397bf94b0a17a6e3393da84
KoNekoD <moskva111@yahoo.com>
2024-06-02 19:25:45 +0300

    fix: 👌 review

--- assets/src/shared/lib/utils.ts
+++ assets/src/shared/lib/utils.ts
@@ -5,24 +5,24 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-const default_name_len =
-  window.innerWidth < 640
-    ? 35
-    : window.innerWidth < 1280
-      ? 55
-      : window.innerWidth < 1536
-        ? 50
-        : window.innerWidth < 1920
-          ? 15
-          : 40;
-
 export function useNameLen(input: string, postfix: string = ''): string {
-  if (input.length <= default_name_len) {
+  const DEFAULT_NAME_LEN =
+    window.innerWidth < 640
+      ? 35
+      : window.innerWidth < 1280
+        ? 55
+        : window.innerWidth < 1536
+          ? 50
+          : window.innerWidth < 1920
+            ? 15
+            : 40;
+
+  if (input.length <= DEFAULT_NAME_LEN) {
     return input;
   }
 
   const postfixLength = postfix ? postfix.length + 1 : 0;
-  const truncatedLength = default_name_len - postfixLength;
+  const truncatedLength = DEFAULT_NAME_LEN - postfixLength;
 
   return input.substring(0, truncatedLength) + '...' + (postfix ? postfix : '');
 }
