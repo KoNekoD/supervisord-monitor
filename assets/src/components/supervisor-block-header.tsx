fb48f6d0236a6101a793588bddb6101c71610c78
KoNekoD <moskva111@yahoo.com>
2024-06-02 19:24:00 +0300

    fix: 👌 review

--- assets/src/components/supervisor-block-header.tsx
+++ assets/src/components/supervisor-block-header.tsx
@@ -1,39 +1,37 @@
-import {IoLockClosedSharp} from 'react-icons/io5';
-import {TfiReload} from 'react-icons/tfi';
-import {RiFileShredLine, RiPlayFill, RiStopFill} from 'react-icons/ri';
-import {ActButton, Type} from './act-button';
-import {AddProgram} from './add-program';
-import {useStore} from '~/main/context-provider';
-import {useNameLen} from "~/shared/lib/utils";
+import { IoLockClosedSharp } from 'react-icons/io5';
+import { TfiReload } from 'react-icons/tfi';
+import { RiFileShredLine, RiPlayFill, RiStopFill } from 'react-icons/ri';
+import { ActButton, Type } from './act-button';
+import { AddProgram } from './add-program';
+import { useStore } from '~/main/context-provider';
+import { useNameLen } from '~/shared/lib/utils';
 
-export const SupervisorBlockHeader = ({item}: { item: ApiSupervisor }) => {
-  const {landingStore} = useStore();
+export const SupervisorBlockHeader = ({ item }: { item: ApiSupervisor }) => {
+  const { landingStore } = useStore();
 
   const buttons = (
     <div className='flex justify-end'>
       <div className='flex items-center space-x-1'>
-        <AddProgram/>
+        <AddProgram />
         <ActButton type={Type.Orange} onClick={() => landingStore.clearAllProcessLog(item.server.name)}>
-          <RiFileShredLine/>
+          <RiFileShredLine />
         </ActButton>
         <ActButton type={Type.Green} onClick={() => landingStore.startAll(item.server.name)}>
-          <RiPlayFill/>
+          <RiPlayFill />
         </ActButton>
         <ActButton type={Type.Red} onClick={() => landingStore.stopAll(item.server.name)}>
-          <RiStopFill/>
+          <RiStopFill />
         </ActButton>
         <ActButton type={Type.Blue} onClick={() => landingStore.restartAll(item.server.name)}>
-          <TfiReload/>
+          <TfiReload />
         </ActButton>
       </div>
     </div>
   );
 
   const errMark = (
-    <div className="flex items-center justify-center">
-      <span className='text-sm font-bold text-red-700'>
-        error
-      </span>
+    <div className='flex items-center justify-center'>
+      <span className='text-sm font-bold text-red-700'>error</span>
     </div>
   );
 
@@ -49,7 +47,7 @@ export const SupervisorBlockHeader = ({item}: { item: ApiSupervisor }) => {
         </a>
         <div className='flex flex-wrap items-center space-x-1'>
           <a title={fullServerTitle}>{useNameLen(serverTitle, serverPort)}</a>
-          <span>{item.ok && item.server.authenticated && <IoLockClosedSharp color='green'/>}</span>
+          <span>{item.ok && item.server.authenticated && <IoLockClosedSharp color='green' />}</span>
           <span>{item.version}</span>
         </div>
       </div>
