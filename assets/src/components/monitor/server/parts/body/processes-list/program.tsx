1f9c1de2fc4567e1c20e3b3202d71478c1da06b1
KoNekoD <moskva111@yahoo.com>
2024-06-04 20:32:01 +0300

    refactor: fix bad code style

--- assets/src/components/monitor/server/parts/body/processes-list/program.tsx
+++ assets/src/components/monitor/server/parts/body/processes-list/program.tsx
@@ -15,25 +15,23 @@ export const Program = ({ group, server }: { group: ApiProcessGroup; server: Api
 
   return (
     <div className='px-2 py-1'>
-      <div className='accordion-item'>
-        <div className='flex items-center space-x-2'>
-          <div>
-            <div className='transititext-secondary flex cursor-pointer items-center space-x-2 text-sm text-gray-500 transition duration-150 ease-in-out hover:text-secondary-600 focus:text-secondary-600 active:text-secondary-700 dark:text-secondary-400 dark:hover:text-secondary-500 dark:focus:text-secondary-500 dark:active:text-secondary-600' onClick={() => setIsActive(!isActive)}>
-              {isActive ? <FaChevronUp className='h-4 w-4' /> : <FaChevronDown className='h-4 w-4' />}
-              <div>
-                <a>{group.name}</a>
-              </div>
-            </div>
-          </div>
-          <div className='flex flex-grow items-center justify-between'>
-            {/* Костыль чтобы кнопки были на всю ширину */} &nbsp;
+      <div className='flex items-center space-x-2'>
+        <div>
+          <div className='transititext-secondary flex cursor-pointer items-center space-x-2 text-sm text-gray-500 transition duration-150 ease-in-out hover:text-secondary-600 focus:text-secondary-600 active:text-secondary-700 dark:text-secondary-400 dark:hover:text-secondary-500 dark:focus:text-secondary-500 dark:active:text-secondary-600' onClick={() => setIsActive(!isActive)}>
+            {isActive ? <FaChevronUp className='h-4 w-4' /> : <FaChevronDown className='h-4 w-4' />}
             <div>
-              <Buttons server={server} group={group} />
+              <a>{group.name}</a>
             </div>
           </div>
         </div>
-        {isActive && <div className='accordion-content'>{items}</div>}
+        <div className='flex flex-grow items-center justify-between'>
+          {/* Костыль чтобы кнопки были на всю ширину */} &nbsp;
+          <div>
+            <Buttons server={server} group={group} />
+          </div>
+        </div>
       </div>
+      {isActive && items}
     </div>
   );
 };
