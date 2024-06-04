bacf14cfb4c65a0ddb4e0c8498e97e4acc47ef24
KoNekoD <moskva111@yahoo.com>
2024-06-04 22:30:46 +0300

    refactor: replace unknown css

--- assets/src/components/monitor/server/parts/body/processes-list/program/process-log.tsx
+++ assets/src/components/monitor/server/parts/body/processes-list/program/process-log.tsx
@@ -30,9 +30,9 @@ export const ProcessLog = ({ process, server }: { process: ApiProcess; server: A
         <div className='fixed left-0 top-0 z-[1055] h-full w-full overflow-y-auto overflow-x-hidden outline-none'>
           <div className='pointer-events-none relative h-full w-full'>
             <div className='pointer-events-auto relative flex max-h-[100%-1rem] w-full flex-col rounded-md border-none bg-clip-padding text-current shadow-lg outline-none'>
-              <div className='m-8 max-h-[100%-1rem] bg-white dark:bg-neutral-600'>
-                <div className='flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50'>
-                  <h5 className='text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200'>Log</h5>
+              <div className='m-8 max-h-[100%-1rem] bg-white dark:bg-gray-600'>
+                <div className='flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-gray-100 border-opacity-100 p-4 dark:border-opacity-50'>
+                  <h5 className='text-xl font-medium leading-normal text-gray-800 dark:text-gray-200'>Log</h5>
                   <button type='button' className='box-content h-10 rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none' onClick={() => setShowModal(false)}>
                     <RiCloseFill />
                   </button>
@@ -42,7 +42,7 @@ export const ProcessLog = ({ process, server }: { process: ApiProcess; server: A
                     <p key={index}>{item}</p>
                   ))}
                 </div>
-                <div className='flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50'>
+                <div className='flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-gray-100 border-opacity-100 p-4 dark:border-opacity-50'>
                   <button type='button' onClick={() => setShowModal(false)} className='background-transparent px-6 py-2 text-sm font-bold uppercase text-red-500 outline-none transition-all duration-150 ease-linear focus:outline-none'>
                     Close
                   </button>
--- assets/src/components/monitor/server/parts/body/processes-list/program/process/status.tsx
+++ assets/src/components/monitor/server/parts/body/processes-list/program/process/status.tsx
@@ -1,14 +1,14 @@
 export const Status = ({ stateName }: { stateName: string }) => {
   switch (stateName) {
     case 'RUNNING':
-      return <span className='inline-block whitespace-nowrap rounded-[0.27rem] bg-success-100 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-success-700'>{stateName}</span>;
+      return <span className='inline-block whitespace-nowrap rounded-[0.27rem] bg-green-100 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-green-700'>{stateName}</span>;
     case 'STARTING':
-      return <span className='inline-block whitespace-nowrap rounded-[0.27rem] bg-info-100 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-info-800'>{stateName}</span>;
+      return <span className='inline-block whitespace-nowrap rounded-[0.27rem] bg-blue-100 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-blue-800'>{stateName}</span>;
     case 'FATAL':
-      return <span className='inline-block whitespace-nowrap rounded-[0.27rem] bg-danger-100 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-danger-700'>{stateName}</span>;
+      return <span className='inline-block whitespace-nowrap rounded-[0.27rem] bg-red-100 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-red-700'>{stateName}</span>;
     case 'STOPPED':
-      return <span className='inline-block whitespace-nowrap rounded-[0.27rem] bg-neutral-800 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-neutral-50 dark:bg-neutral-900'>{stateName}</span>;
+      return <span className='inline-block whitespace-nowrap rounded-[0.27rem] bg-gray-800 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-gray-50 dark:bg-gray-900'>{stateName}</span>;
     default:
-      return <span className='inline-block whitespace-nowrap rounded-[0.27rem] bg-warning-100 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-warning-800'>{stateName}</span>;
+      return <span className='inline-block whitespace-nowrap rounded-[0.27rem] bg-yellow-100 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-yellow-800'>{stateName}</span>;
   }
 };
