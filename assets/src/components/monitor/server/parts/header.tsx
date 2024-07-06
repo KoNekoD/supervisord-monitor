fd3b3d5f811ccd48397d1d0be30c9034509c6b6f
lerothias <lerothias@yahoo.com>
2024-07-06 20:10:53 +0300

    feat: :sparkles: move all operations to custom hook

--- assets/src/components/monitor/server/parts/header.tsx
+++ assets/src/components/monitor/server/parts/header.tsx
@@ -1,79 +1,10 @@
 import { Buttons } from '~/components/monitor/server/parts/header/buttons';
 import { ServerInfo } from '~/components/monitor/server/parts/header/server-info';
 import { ServerTitle } from '~/components/monitor/server/parts/header/server-title';
-import { checkValidResultSuccess } from '~/shared/lib/checkValidResultSuccess';
-import toast from 'react-hot-toast';
-import { notifyError } from '~/shared/lib/notifyError';
-import { useManageSupervisors } from '~/api/use-manage-supervisors';
-import { useInvalidateSupervisors } from '~/api/use-get-supervisors';
+import { useOperationsSupervisors } from '~/shared/hooks/use-operations-supervisors';
 
 export const Header = ({ item }: { item: ApiSupervisor }) => {
-  const manageSupervisors = useManageSupervisors();
-  const invalidateSupervisors = useInvalidateSupervisors();
-
-  const clearAllProcessLog = (serverName: string) =>
-    manageSupervisors
-      .mutateAsync({
-        server: serverName,
-        type: 'clear_all_process_log',
-        group: null,
-        process: null,
-      })
-      .then(result => {
-        if (checkValidResultSuccess(result)) {
-          toast.success(`All process logs cleared on server ${serverName}`);
-        }
-        invalidateSupervisors();
-      })
-      .catch(err => notifyError(err));
-
-  const startAll = (serverName: string) =>
-    manageSupervisors
-      .mutateAsync({
-        server: serverName,
-        type: 'start_all_processes',
-        group: null,
-        process: null,
-      })
-      .then(result => {
-        if (checkValidResultSuccess(result)) {
-          toast.success(`All processes started on server ${serverName}`);
-        }
-        invalidateSupervisors();
-      })
-      .catch(err => notifyError(err));
-
-  const stopAll = (serverName: string) =>
-    manageSupervisors
-      .mutateAsync({
-        server: serverName,
-        type: 'stop_all_processes',
-        group: null,
-        process: null,
-      })
-      .then(result => {
-        if (checkValidResultSuccess(result)) {
-          toast.success(`All processes stopped on server ${serverName}`);
-        }
-        invalidateSupervisors();
-      })
-      .catch(err => notifyError(err));
-
-  const restartAll = (serverName: string) =>
-    manageSupervisors
-      .mutateAsync({
-        server: serverName,
-        type: 'restart_all_processes',
-        group: null,
-        process: null,
-      })
-      .then(result => {
-        if (checkValidResultSuccess(result)) {
-          toast.success(`All processes restarted on server ${serverName}`);
-        }
-        invalidateSupervisors();
-      })
-      .catch(err => notifyError(err));
+  const { restartAll, stopAll, startAll, clearAllProcessLog } = useOperationsSupervisors();
 
   return (
     <div className='rounded-t-xl border-b border-l-2 border-r-2 border-t-2 border-gray-200 px-2 py-1'>
