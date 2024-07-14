ee5a960b7954d286825c008942fce84066e4f066
KoNekoD <moskva111@yahoo.com>
2024-07-14 23:48:01 +0300

    fix: 👌 review

--- assets/src/shared/lib/notify-error.ts
+++ assets/src/shared/lib/notify-error.ts
@@ -2,7 +2,7 @@ import { toastManager } from '~/shared/lib/toastManager';
 
 export const notifyError = (err: any) => {
   const heading = err.response.data.title ?? err.message ?? 'Something went wrong: '
-  const details = err.response.data.detail ?? 'No details'
+  const details = err.response.data.detail ?? ''
 
   toastManager.error(heading + '\n' + details)
 }
