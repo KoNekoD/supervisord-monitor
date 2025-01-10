96e3aa085559f2a5fd3fbd6bb8536e4fc5443e89
KoNekoD <moskva111@yahoo.com>
2025-01-10 10:40:26 +0300

    Revert "feat: :sparkles: migrate process operations"
    This reverts commit 4c32fd8c

--- assets/src/components/monitor/server/parts/body/processes-list/program/process/buttons.tsx
+++ assets/src/components/monitor/server/parts/body/processes-list/program/process/buttons.tsx
@@ -1,6 +1,6 @@
+import { useStore } from '~/main/context-provider';
 import { RiPlayFill, RiStopFill } from 'react-icons/ri';
 import { TfiReload } from 'react-icons/tfi';
-import { useOperationsSupervisors } from '~/shared/hooks/use-operations-supervisors';
 import { Fragment } from 'react';
 import { isHasRoleManager } from '~/app/providers/session/context';
 
@@ -9,22 +9,18 @@ export const Buttons = ({ process, server }: { process: ApiProcess; server: ApiS
     return <Fragment />;
   }
 
-  const { startProcess, stopProcess, restartProcess } = useOperationsSupervisors();
-
+  const { landingStore } = useStore();
   const inactiveStates = ['STOPPED', 'EXITED', 'FATAL'];
 
   if (process.stateName === 'RUNNING') {
     return (
       <div className='flex space-x-1'>
-        <button
-          className='rounded bg-red-500 p-2 text-white'
-          onClick={stopProcess.bind(null, server.name, process.group, process.name)}
-        >
+        <button className='rounded bg-red-500 p-2 text-white' onClick={() => landingStore.stopProcess(server, process)}>
           <RiStopFill />
         </button>
         <button
           className='rounded bg-blue-500 p-2 text-white'
-          onClick={restartProcess.bind(null, server.name, process.group, process.name)}
+          onClick={() => landingStore.restartProcess(server, process)}
         >
           <TfiReload />
         </button>
@@ -37,7 +33,7 @@ export const Buttons = ({ process, server }: { process: ApiProcess; server: ApiS
       <div className='flex space-x-1'>
         <button
           className='rounded bg-green-500 p-2 text-white'
-          onClick={startProcess.bind(null, server.name, process.group, process.name)}
+          onClick={() => landingStore.startProcess(server, process)}
         >
           <RiPlayFill />
         </button>
