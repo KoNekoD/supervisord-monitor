fcf1380642df14ae392c74738496132795c70050
KoNekoD <moskva111@yahoo.com>
2025-01-10 10:39:56 +0300

    Revert "feat: :sparkles: migrate process groups operations"
    This reverts commit 6bc893f6396bdb789787803a88ee7e8a2f0c62b0.

--- assets/src/components/monitor/server/parts/body/processes-list/program/buttons/inactive.tsx
+++ assets/src/components/monitor/server/parts/body/processes-list/program/buttons/inactive.tsx
@@ -1,15 +1,12 @@
+import { useStore } from '~/main/context-provider';
 import { RiPlayFill } from 'react-icons/ri';
-import { useOperationsSupervisors } from '~/shared/hooks/use-operations-supervisors';
 
 export const Inactive = ({ server, group }: { server: ApiSupervisorServer; group: ApiProcessGroup }) => {
-  const { startProcessGroup } = useOperationsSupervisors();
+  const { landingStore } = useStore();
 
   return (
     <div className='flex space-x-1'>
-      <button
-        className='rounded bg-green-500 p-2 text-white'
-        onClick={startProcessGroup.bind(null, server.name, group.name)}
-      >
+      <button className='rounded bg-green-500 p-2 text-white' onClick={() => landingStore.startProcessGroup(server.name, group.name)}>
         <RiPlayFill />
       </button>
     </div>
--- assets/src/components/monitor/server/parts/body/processes-list/program/buttons/running.tsx
+++ assets/src/components/monitor/server/parts/body/processes-list/program/buttons/running.tsx
@@ -1,22 +1,16 @@
 import { RiStopFill } from 'react-icons/ri';
 import { TfiReload } from 'react-icons/tfi';
-import { useOperationsSupervisors } from '~/shared/hooks/use-operations-supervisors';
+import { useStore } from '~/main/context-provider';
 
 export const Running = ({ server, group }: { server: ApiSupervisorServer; group: ApiProcessGroup }) => {
-  const { stopProcessGroup, restartProcessGroup } = useOperationsSupervisors();
+  const { landingStore } = useStore();
 
   return (
     <div className='flex space-x-1'>
-      <button
-        className='rounded bg-red-500 p-2 text-white'
-        onClick={stopProcessGroup.bind(null, server.name, group.name)}
-      >
+      <button className='rounded bg-red-500 p-2 text-white' onClick={() => landingStore.stopProcessGroup(server.name, group.name)}>
         <RiStopFill />
       </button>
-      <button
-        className='rounded bg-blue-500 p-2 text-white'
-        onClick={restartProcessGroup.bind(null, server.name, group.name)}
-      >
+      <button className='rounded bg-blue-500 p-2 text-white' onClick={() => landingStore.restartProcessGroup(server.name, group.name)}>
         <TfiReload />
       </button>
     </div>
