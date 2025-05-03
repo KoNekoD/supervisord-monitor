73f9b4aba41ae4550bb76e4377ac0f9e7ee31e3c
KoNekoD <moskva111@yahoo.com>
2025-05-03 20:06:42 +0400

    fix: database provision

---
+++ bin/database_provision.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Create database if it does not exist
+if [ ! -f var/data_dev.db ]; then
+  php bin/console doctrine:database:create
+fi
+
+php bin/console doctrine:migrations:migrate -n
