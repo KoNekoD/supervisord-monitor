d0b95f47a16f20c75034c145fc4a2a8312db873f
KoNekoD <moskva111@yahoo.com>
2025-05-03 20:57:46 +0400

    fix: logic

--- assets/src/hooks/useSettings.ts
+++ assets/src/hooks/useSettings.ts
@@ -2,42 +2,50 @@ import { useLocalStorage } from '~/hooks/useLocalStorage';
 import { Theme } from '~/config/theme';
 import { useEffect } from 'react';
 
+const defaultSettings = {
+  autoRefresh: false,
+  autoRefreshInterval: 10,
+  syncRefresh: false,
+  theme: Theme.system,
+  allowMutators: false,
+};
+
 export const useSettings = () => {
-  const [autoRefresh, setAutoRefresh] = useLocalStorage('auto-refresh', false);
-  const [autoRefreshInterval, setAutoRefreshInterval] = useLocalStorage('auto-refresh-interval', 10);
-  const [syncRefresh, setSyncRefresh] = useLocalStorage('sync-refresh', false);
-  const [theme, setTheme] = useLocalStorage('theme', Theme.system);
-  const [allowMutators, setAllowMutators] = useLocalStorage('allow-mutators', false);
+  const [settings, setSettings] = useLocalStorage('settings', defaultSettings);
 
   useEffect(() => {
     const root = window.document.documentElement;
 
-    if (theme !== Theme.system && root.classList.contains(theme)) {
+    if (settings.theme !== Theme.system && root.classList.contains(settings.theme)) {
       return;
     }
 
     root.classList.remove('light', 'dark');
 
-    if (theme === Theme.system) {
+    if (settings.theme === Theme.system) {
       const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 
       root.classList.add(systemTheme);
       return;
     }
 
-    root.classList.add(theme);
-  }, [theme]);
+    root.classList.add(settings.theme);
+  }, [settings.theme]);
 
   return {
-    autoRefresh,
-    setAutoRefresh,
-    autoRefreshInterval,
-    setAutoRefreshInterval,
-    syncRefresh,
-    setSyncRefresh,
-    theme,
-    setTheme,
-    allowMutators,
-    setAllowMutators,
+    autoRefresh: settings.autoRefresh,
+    setAutoRefresh: (v: boolean) => setSettings({ ...settings, autoRefresh: v }),
+
+    autoRefreshInterval: settings.autoRefreshInterval,
+    setAutoRefreshInterval: (v: number) => setSettings({ ...settings, autoRefreshInterval: v }),
+
+    syncRefresh: settings.syncRefresh,
+    setSyncRefresh: (v: boolean) => setSettings({ ...settings, syncRefresh: v }),
+
+    theme: settings.theme,
+    setTheme: (v: Theme) => setSettings({ ...settings, theme: v }),
+
+    allowMutators: settings.allowMutators,
+    setAllowMutators: (v: boolean) => setSettings({ ...settings, allowMutators: v }),
   };
 };
